bc0a3c85fab0f6346d7780af2341ff7803b1debc
Gil BMP <94876642+gilbmporto@users.noreply.github.com>
2022-07-27 10:37:37 -0300

    Versão 1.0 do Super Encoder
    Ainda não está funcionando. (Problema no export/import?)

---
+++ encoder.js
@@ -0,0 +1,31 @@
+import { caesarCipher, symbolCipher, reverseCipher } from './encryptors';
+
+//Declare the functions to encript and decript the message:
+const encodeMessage = (str) => {
+    return caesarCipher(reverseCipher(symbolCipher(str)), 3);
+};
+
+const decodeMessage = (str) => {
+    return symbolCipher(reverseCipher(caesarCipher(str, -3)));
+};
+
+//Declare the HTML elements:
+var mensagemACriptografar = document.getElementById("mens-a-criptografar");
+var mensagemCriptografada = document.getElementById("mens-cripto");
+const botaoCriptografar = document.getElementById("cripto-mens");
+const botaoDescriptografar = document.getElementById("decript-mens");
+var mensRecomendacao = document.getElementById("recomendacao");
+
+//Add event listeners and make the website functional:
+botaoCriptografar.addEventListener('click', () => {
+    let cipheredMessage = encodeMessage((mensagemACriptografar.value));
+    mensagemCriptografada.innerHTML = cipheredMessage;
+    mensRecomendacao.style.display = 'block';
+});
+
+botaoDescriptografar.addEventListener('click', () => {
+    let decipheredMessage = decodeMessage((mensagemACriptografar.value).toSring());
+    mensagemCriptografada.innerHTML = decipheredMessage;
+    mensRecomendacao.innerHTML = 'Mensagem acima descriptografada! 🔓';
+    mensRecomendacao.style.display = 'block';
+});
