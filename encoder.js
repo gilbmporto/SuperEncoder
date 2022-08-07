46c619537f2bfeecf12e1bce2093ba5f5fba1ace
Gil BMP <94876642+gilbmporto@users.noreply.github.com>
2022-08-07 18:52:44 -0300

    2nd Commit - Improved Encoder App
    - Add new animations (loading bar) everytime the user press one of the buttons to encrypt or decrypt any message;
    - Add async functions and promises to perform the new animations.

--- encoder.js
+++ encoder.js
@@ -15,7 +15,79 @@ var mensagemCriptografada = document.getElementById("mens-cripto");
 const botaoCriptografar = document.getElementById("cripto-mens");
 const botaoDescriptografar = document.getElementById("decript-mens");
 var mensRecomendacao = document.getElementById("recomendacao");
+const LoadingBar = document.getElementById('theInsideLoadBar');
+const OutterLoadingBar = document.getElementById('theOutsideLoadBar');
+var loadingTxt = document.getElementById("loadingTxt");
 
+//A promise function to make the HTML content dynamic:
+const modificarElementos = (tempo, func) => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(func());
+        }, tempo)
+    }) 
+};
+
+//The main async function make this webapp purpose possible:
+const encryptItNow = async () => {
+    let firstAction = await modificarElementos(0, () => {
+        OutterLoadingBar.style.display = 'block';
+    });
+    console.log(firstAction);
+    let secondAction = await modificarElementos(500, () => {
+        LoadingBar.style.width = '100%';
+    });
+    console.log(secondAction);
+    let thirdAction = await modificarElementos(300, () => {
+        loadingTxt.innerHTML = 'Loading';
+    });
+    console.log(thirdAction);
+    let fourthAction = await modificarElementos(1300, () => {
+        let cipheredMessage = encodeMessage((mensagemACriptografar.value));
+        mensagemCriptografada.innerHTML = cipheredMessage;
+        mensRecomendacao.style.display = 'block';
+    });
+    console.log(fourthAction);
+    let finalAction = await modificarElementos(200, () => {
+        OutterLoadingBar.style.display = 'none';
+        LoadingBar.style.width = '0%';
+        loadingTxt.innerHTML = '';
+    });
+    console.log(finalAction);
+};
+
+const decryptItNow = async () => {
+    let firstAction = await modificarElementos(0, () => {
+        OutterLoadingBar.style.display = 'block';
+    });
+    console.log(firstAction);
+    let secondAction = await modificarElementos(500, () => {
+        LoadingBar.style.width = '100%';
+    });
+    console.log(secondAction);
+    let thirdAction = await modificarElementos(300, () => {
+        loadingTxt.innerHTML = 'Loading';
+    });
+    console.log(thirdAction);
+    let fourthAction = await modificarElementos(1300, () => {
+        let decipheredMessage = decodeMessage(mensagemACriptografar.value);
+        mensagemCriptografada.innerHTML = decipheredMessage;
+        mensRecomendacao.innerHTML = 'Mensagem acima descriptografada! 🔓';
+        mensRecomendacao.style.display = 'block';
+    });
+    console.log(fourthAction);
+    let finalAction = await modificarElementos(200, () => {
+        OutterLoadingBar.style.display = 'none';
+        LoadingBar.style.width = '0%';
+        loadingTxt.innerHTML = '';
+    });
+    console.log(finalAction);
+};
+
+botaoCriptografar.addEventListener('click', encryptItNow);
+botaoDescriptografar.addEventListener('click', decryptItNow);
+
+/*
 //Add event listeners and make the website functional:
 botaoCriptografar.addEventListener('click', () => {
     let cipheredMessage = encodeMessage((mensagemACriptografar.value));
@@ -29,3 +101,4 @@ botaoDescriptografar.addEventListener('click', () => {
     mensRecomendacao.innerHTML = 'Mensagem acima descriptografada! 🔓';
     mensRecomendacao.style.display = 'block';
 });
+*/
